3fa8bccfe98da81d1b298560907f8d55901526c4
azuryte5 <85147307+azuryte5@users.noreply.github.com>
2021-09-25 00:54:14 -0400

    played around with attr id's and adding an id to an already made id. will have to try commment on line 23

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,41 @@
+// This is the content within the text box
 var work=$("#hour-9").val();
-// Old time is greyed out
-// current time is red
-// future time is green
-//icon for save button
-//center header
 
-//today
 
+// This shows the date that gets posted on the page
 var currentDay = moment().format("dddd, MMMM Do YYYY")
 console.log(currentDay)
 $("#currentDay").append(currentDay);
 
+var count = []
+for (var i = 9; i < 18; i++){
+    count.push({
+    id: i,
+    text:"placeholder"
+    });
+};
+console.log(count[0])
+$( "#hour-" ).attr( "id", "#hour-" + count[0].id)
+//<textarea id="hour-9" class="col description"></textarea>
+//             id="hour-"+ count[i]
 
+// DAMN IT DIDn't WORK
+// I need to add a complete ID to the tags and then call and save them as whole. I wasn't able to refer to a split half id.
 
-// day = new Date()
-// var hour = day.getHours()
-// console.log(hour)
+//$( "#hour-" ).attr( "id", "#hour-"+count[0] );
+//each?
+//find?
+//trim?
+//attr?
+
+//count array built up to 8 the length of the table
 
 //plannerArray = [ 9,10,11 ,12 ,13 ,14 ,15,16.17 ]
 // console.log(plannerArray)
 
 // console.log(plannerArray[7]===hour)
 
-// if (plannerArray[0]===hour) {
-// $("hour-"+plannerArray[0]).addClass("present")
-// }
-
-// //this works? wow.
-// var hourlyTimer = function (hour) {
-// // var everyHour = setInterval(function() {
-
 // plannerArray = [ 9,10,11 ,12 ,13 ,14 ,15,16];
-// var hour = day.getHours();   
 
 // for (var i=0; i > plannerArray.length; i++){
 // if (plannerArray[i]===hour) {
@@ -58,62 +62,21 @@ $("#hour-9").addClass("future")
     $("#hour-9").addClass("past")    
 }
 
+// if (moment().isAfter(time)) {
+//     $(taskEl).addClass("list-group-item-danger");
+//   } else if (Math.abs(moment().diff(time, "days")) <= 2) {
+//     $(taskEl).addClass("list-group-item-warning");
+//   }
 
-// var time =hourlyTimer(hour).value
-// console.log(time)
-// // }
 // // var everyHour =function(){setInterval(hourlyTimer(),5000)};
 // everyHour()
-      // Declares a 'list' variable that holds the parsed to-do items retrieved from 'localStorage'
-    //   // If there is nothing in 'localStorage', sets the 'list' to an empty array
-    //   var list = JSON.parse(localStorage.getItem('hour')) || [];
-
-    //   // Renders our to-dos to the page
-    //   function renderTodos(list) {
-    //     // Empties out the html
-    //     $('#to-dos').empty();
-
-    //     // Iterates over the 'list'
-    //     for (var i = 0; i < list.length; i++) {
-    //       // Creates a new variable 'toDoItem' that will hold a "<p>" tag
-    //       // Sets the `list` item's value as text of this <p> element
-    //       var toDoItem = $('<p>');
-    //       toDoItem.text(list[i]);
-
-    //       // Creates a button `toDoClose` with an attribute called `data-to-do` and a unique `id`
-    //       var toDoClose = $('<button>');
-    //       toDoClose.attr('data-to-do', i);
-
-    //       // Gives the button a class called 'checkbox'
-    //       toDoClose.addClass('checkbox');
-
-    //       // Adds a checkmark symbol as its text value
-    //       toDoClose.text('✓');
-
-    //       // Adds the button to the `toDoItem`
-    //       toDoItem = toDoItem.prepend(toDoClose);
-
-    //       // Adds 'toDoItem' to the To-Do List div
-    //       $('#to-dos').append(toDoItem);
-    //     }
-    //   }
-// $("hour-9").storeText(function() {
-// var value = $( this ).val();
-// console.log(value);
-// })
-// .storeText();
-
-// // This two give the same result
-// var declare = $("#hour-9").val()
-// console.log(declare)
-//save button works
 
 //CAN I BUILD AN ARRAY FOR ALL OF THIS?
 $("#button-9").on("click", function(){
     //Work makes it store the text input of the log entry. finally. val kinda uses the textcontent
     work=$("#hour-9").val()
     console.log(work)
-    $("#hour").html("<p class='text-success'>Saving Successful</p>");
+    $("#hour").html("<p class='text-success'>Saving Successful ✓</p>");
     setTimeout(function() {
     $("#hour").html("9 am")    
     }, 2000);
