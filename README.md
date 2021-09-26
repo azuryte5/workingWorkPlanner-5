389dd81d6abaeb687919eadb0d9cef87647c2809
azuryte5 <85147307+azuryte5@users.noreply.github.com>
2021-09-25 21:06:02 -0400

    readme and touch ups on code

--- README.md
+++ README.md
@@ -1,40 +1,44 @@
 # workingWorkPlanner-5
 05 Third-Party APIs: Work Day Scheduler
-Create a simple calendar application that allows the user to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
-The starter code uses the Moment.js library to work with date and time, but feel free to use a different JavaScript solution to handle this functionality since Moment.js is considered a "legacy" project. Learn more about these other solutions in the Moment.js project status page.
 
-User Story
+Link to Github Application: To be added!
+
+Link to Githut Repository: https://github.com/azuryte5/workingWorkPlanner-5
+
+<img src="assets/images/Screenshot5.png" alt="Work day planner" width="500" style="max-width: 100%;">
+
+##Project Overview:
+
+For this assignment we were given start code and had to make use of a pre-made CSS style sheet and code a javascript for functionality.
+
+I tried to include learning of jquery into the code and found some creative ways to put together desired functionality. When the page is loaded, the date is presented and user can add events, routines into a textarea and click save to view content later. When page reloads, information is retained and presented for further use. 
+
+Line 23-31 of Javascript decides which time blocks to turn red,green or grey. 
+
+I had a bug where the program fails to run after work hours and chose to add line 17 to prevent program breaking. I was beginning to refactor the code to get all content to be contained in the count array but I was hoping to complete Challenge 6 before Project 1. 
+
+I kept some of my old code, to show where I cleaned up some code. Notably the buttons and Load States.
+
+
+* User Story
 AS AN employee with a busy schedule
 I WANT to add important events to a daily planner
 SO THAT I can manage my time effectively
-✔️
-Acceptance Criteria
+
+* Acceptance Criteria
 GIVEN I am using a daily planner to create a schedule
-[✔️ ]WHEN I open the planner
+[✔️ ] WHEN I open the planner
 THEN the current day is displayed at the top of the calendar
-[✔️ ]WHEN I scroll down
+[✔️ ] WHEN I scroll down
 THEN I am presented with timeblocks for standard business hours
-[✔️ ]WHEN I view the timeblocks for that day
+[✔️ ] WHEN I view the timeblocks for that day
 THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-[✔️]WHEN I click into a timeblock
+[✔️] WHEN I click into a timeblock
 THEN I can enter an event
-[ ✔️]WHEN I click the save button for that timeblock
+[✔️] WHEN I click the save button for that timeblock
 THEN the text for that event is saved in local storage
-[✔️ ]WHEN I refresh the page
+[✔️ ] WHEN I refresh the page
 THEN the saved events persist
-
-
-The following animation demonstrates the application functionality:
-
-
-Review
-You are required to submit the following for review:
-
-
-The URL of the deployed application.
-
-
-The URL of the GitHub repository. Give the repository a unique name and include a README describing the project.
 
 
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,7 @@
-
+// Getting today's date for the header
 var currentDay = moment().format("dddd, MMMM Do YYYY")
 console.log(currentDay)
 $("#currentDay").append(currentDay);
-
-// If there is time. I want to Assign all the id's from this array with each section
-//change to section? add to div
-
-//each?
-//find?
-//trim?
-//attr? change and over
 
 // hour generates the number of the current hour
 var hour = new Date().getHours()
@@ -22,11 +14,13 @@ console.log("The current time with am or pm is: " +actualHour)
 //Current time helps locate which classes to give to colours too
 var currentTime = document.getElementById("hour-"+hour)
 
+//I encountered a bug where getElementbyId and text.content would fail if there was no id on html. I didn't want to change it back to queryselector so I 
+// used stop bug to keep hour as a string so it would keep a condition check working on line 25. This assigment works during 9-5 though! 😃
 if (hour > 17) {currentTime="Stop Bug"}
 
 console.log("The current time of " +currentTime.textContent+ " will be red")
 
-// This for loop will make the current time red if applicable, times that haven't occured in green and past events in grey.
+// This for loop will make the current time red if applicable, times that haven't occurred will be green and past events in grey.
 for (var i = 9; i < 18; i++){ 
 if (currentTime.textContent === actualHour) {
 $("#text-"+hour).addClass("present")
@@ -37,19 +31,17 @@ $("#text-"+i).addClass("future")
     $("#text-"+i).addClass("past")    
 }}
 
-// If i have time I'm trying to build all id's in a count array 
-var count = []
-for (var i = 9; i < 18; i++){
-    count.push({
-    id: "hour-"+ i,
-    text: "text-" +i,
-    button: "button-" +i,
-    textArea: " "});
-
-
-};
+// // I spent some time trying to build an array to store all id's and data entries but ran out of time. I did get some other code cleaned up and sped up a bit.
+// var count = []
+// for (var i = 9; i < 18; i++){
+//     count.push({
+//     id: "hour-"+ i,
+//     text: "text-" +i,
+//     button: "button-" +i,
+//     textArea: " "});
+// };
 console.log(count)
-// each save button will target it's sibiling .description and save it's textarea and post a display message for 2seconds
+// each save button will target it's sibling .description and save it's textarea and post a display message for 2seconds
 $(".saveBtn").on("click", function(){
     var slotNumber = $(this).parent().attr("id");
     var textArea = $(this).siblings(".description").val();
@@ -59,7 +51,7 @@ $(".saveBtn").on("click", function(){
         }, 2000);
     localStorage.setItem(slotNumber, textArea);
     });
-
+// Old code that was the long form of each button click. I was able to work out a quicker way.
 // $("#button-9").on("click", function(){
 //     //Work makes it store the text input of the log entry. finally. val kinda uses the textcontent
 //     work=$("#text-9").val()
@@ -71,93 +63,7 @@ $(".saveBtn").on("click", function(){
 //     localStorage.setItem("9amSlot", JSON.stringify(work));
 //     // saveTasks(work)
 // })
-
-
-// $("#button-10").on("click", function(){
-//     //Work makes it store the text input of the log entry. finally. val kinda uses the textcontent
-//     work=$("#text-10").val()
-//     console.log(work)
-//     $("#hour-10").html("<p class='text-success'>Saving Successful ✓</p>");
-//     setTimeout(function() {
-//     $("#hour-10").html("10am")    
-//     }, 2000);
-//     localStorage.setItem("10amSlot", JSON.stringify(work));
-//     // saveTasks(work)
-// })
-
-// $("#button-11").on("click", function(){
-//     //Work makes it store the text input of the log entry. finally. val kinda uses the textcontent
-//     work=$("#text-11").val()
-//     console.log(work)
-//     $("#hour-11").html("<p class='text-success'>Saving Successful ✓</p>");
-//     setTimeout(function() {
-//     $("#hour-11").html("11am")    
-//     }, 2000);
-//     localStorage.setItem("11amSlot", JSON.stringify(work));
-//     // saveTasks(work)
-// })
-
-
-// $("#button-12").on("click", function(){
-//     //Work makes it store the text input of the log entry. finally. val kinda uses the textcontent
-//     work=$("#text-12").val()
-//     console.log(work)
-//     $("#hour-12").html("<p class='text-success'>Saving Successful ✓</p>");
-//     setTimeout(function() {
-//     $("#hour-12").html("12pm")    
-//     }, 2000);
-//     localStorage.setItem("12pmSlot", JSON.stringify(work));
-//     // saveTasks(work)
-// })
-
-// $("#button-13").on("click", function(){
-//     //Work makes it store the text input of the log entry. finally. val kinda uses the textcontent
-//     work=$("#text-13").val()
-//     console.log(work)
-//     $("#hour-13").html("<p class='text-success'>Saving Successful ✓</p>");
-//     setTimeout(function() {
-//     $("#hour-13").html("1pm")    
-//     }, 2000);
-//     localStorage.setItem("1pmSlot", JSON.stringify(work));
-//     // saveTasks(work)
-// })
-
-
-// $("#button-14").on("click", function(){
-//     //Work makes it store the text input of the log entry. finally. val kinda uses the textcontent
-//     work=$("#text-14").val()
-//     console.log(work)
-//     $("#hour-14").html("<p class='text-success'>Saving Successful ✓</p>");
-//     setTimeout(function() {
-//     $("#hour-14").html("2pm")    
-//     }, 2000);
-//     localStorage.setItem("2pmSlot", JSON.stringify(work));
-//     // saveTasks(work)
-// })
-
-// $("#button-15").on("click", function(){
-//     //Work makes it store the text input of the log entry. finally. val kinda uses the textcontent
-//     work=$("#text-15").val()
-//     console.log(work)
-//     $("#hour-15").html("<p class='text-success'>Saving Successful ✓</p>");
-//     setTimeout(function() {
-//     $("#hour-15").html("3pm")    
-//     }, 2000);
-//     localStorage.setItem("3pmSlot", JSON.stringify(work));
-//     // saveTasks(work)
-// })
-
-// $("#button-16").on("click", function(){
-//     //Work makes it store the text input of the log entry. finally. val kinda uses the textcontent
-//     work=$("#text-16").val()
-//     console.log(work)
-//     $("#hour-16").html("<p class='text-success'>Saving Successful ✓</p>");
-//     setTimeout(function() {
-//     $("#hour-16").html("4pm")    
-//     }, 2000);
-//     localStorage.setItem("4pmSlot", JSON.stringify(work));
-//     // saveTasks(work)
-// })
+// buttons 10 - 16
 
 // $("#button-17").on("click", function(){
 //     //Work makes it store the text input of the log entry. finally. val kinda uses the textcontent
@@ -170,6 +76,8 @@ $(".saveBtn").on("click", function(){
 //     localStorage.setItem("5pmSlot", JSON.stringify(work));
 //     // saveTasks(work)
 // })
+
+// I was able to build a clean function to store data.
 var loadTasks =function(){}
 for (var i = 9; i < 18; i++){
 $("#text-"+i).val(localStorage.getItem("slot-"+i));
